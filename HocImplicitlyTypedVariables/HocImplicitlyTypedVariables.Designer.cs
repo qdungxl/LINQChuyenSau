5abbdc97cbb97d40b9b7c793b4523ca312b3af37
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-03-26 16:14:02 +0900

    Add ImplicitlyTypedVariables

---
+++ HocImplicitlyTypedVariables/HocImplicitlyTypedVariables.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace HocImplicitlyTypedVariables
+{
+    partial class HocImplicitlyTypedVariables
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblMess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tạo các implicitType";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMess
+            // 
+            this.lblMess.AutoSize = true;
+            this.lblMess.Location = new System.Drawing.Point(12, 108);
+            this.lblMess.Name = "lblMess";
+            this.lblMess.Size = new System.Drawing.Size(51, 20);
+            this.lblMess.TabIndex = 1;
+            this.lblMess.Text = "label1";
+            // 
+            // HocImplicitlyTypedVariables
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 314);
+            this.Controls.Add(this.lblMess);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "HocImplicitlyTypedVariables";
+            this.Text = "Học Implicitly Typed Variables";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblMess;
+    }
+}
+
---
+++ HocImplicitlyTypedVariables/HocImplicitlyTypedVariables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HocImplicitlyTypedVariables
+{
+    public partial class HocImplicitlyTypedVariables : Form
+    {
+        public HocImplicitlyTypedVariables()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var x = 113;
+            var y = 5.5;
+            var z = 7.5f;
+            var s = "Obama";
+            var d = new DateTime(2022, 3, 26);
+            lblMess.Text = y.GetType().ToString();
+        }
+    }
+}
