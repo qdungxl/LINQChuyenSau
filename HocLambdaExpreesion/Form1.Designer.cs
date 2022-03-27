b55fc91cb14aec83a5a2417942a335bee0ef5152
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-03-27 11:50:11 +0900

    Add Lambda Expression

---
+++ HocLambdaExpreesion/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace HocLambdaExpreesion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(12, 24);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(278, 51);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test Lambda Expression";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTest);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Học Lambda Expression";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HocLambdaExpreesion/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HocLambdaExpreesion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public delegate int ChangeInt(int x);
+        public int Tang2(int x)
+        {
+            return x + 2;
+        }
+        public delegate int TongDelegate(int x, int y);
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            ChangeInt d1 = new ChangeInt(Tang2);
+            ChangeInt d2 = ((int x) => x + 2);
+            ChangeInt d3 = x => x + 2;
+            ChangeInt d4 = delegate (int x)
+            {
+                return x + 2;
+            };
+            //label1.Text = d3(100) + "";
+            TongDelegate d5 = (x, y) => x + y;
+            //label1.Text = d5(3, 4) + "";
+            TongDelegate d6 = (x, y) => x - y;
+            label1.Text = d6(3, 4) + "";
+        }
+    }
+}
