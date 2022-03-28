833e342777c4b30538b0928e171d452c329d051b
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-03-28 21:24:20 +0900

    Add Find

---
+++ HocFind/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace HocFind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstN = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTim = new System.Windows.Forms.TextBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.lvSanPham = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDonGia = new System.Windows.Forms.TextBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnTimDonGia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N:";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(154, 17);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(287, 26);
+            this.txtN.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tạo DS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstN
+            // 
+            this.lstN.FormattingEnabled = true;
+            this.lstN.ItemHeight = 20;
+            this.lstN.Location = new System.Drawing.Point(119, 130);
+            this.lstN.Name = "lstN";
+            this.lstN.Size = new System.Drawing.Size(322, 184);
+            this.lstN.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tìm:";
+            // 
+            // txtTim
+            // 
+            this.txtTim.Location = new System.Drawing.Point(150, 354);
+            this.txtTim.Name = "txtTim";
+            this.txtTim.Size = new System.Drawing.Size(165, 26);
+            this.txtTim.TabIndex = 5;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(344, 346);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(93, 34);
+            this.btnTim.TabIndex = 6;
+            this.btnTim.Text = "Tim";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // lvSanPham
+            // 
+            this.lvSanPham.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvSanPham.FullRowSelect = true;
+            this.lvSanPham.GridLines = true;
+            this.lvSanPham.HideSelection = false;
+            this.lvSanPham.Location = new System.Drawing.Point(531, 23);
+            this.lvSanPham.Name = "lvSanPham";
+            this.lvSanPham.Size = new System.Drawing.Size(458, 243);
+            this.lvSanPham.TabIndex = 7;
+            this.lvSanPham.UseCompatibleStateImageBehavior = false;
+            this.lvSanPham.View = System.Windows.Forms.View.Details;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(530, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Đơn giá:";
+            // 
+            // txtDonGia
+            // 
+            this.txtDonGia.Location = new System.Drawing.Point(604, 313);
+            this.txtDonGia.Name = "txtDonGia";
+            this.txtDonGia.Size = new System.Drawing.Size(201, 26);
+            this.txtDonGia.TabIndex = 9;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Đơn Giá";
+            // 
+            // btnTimDonGia
+            // 
+            this.btnTimDonGia.Location = new System.Drawing.Point(858, 313);
+            this.btnTimDonGia.Name = "btnTimDonGia";
+            this.btnTimDonGia.Size = new System.Drawing.Size(119, 26);
+            this.btnTimDonGia.TabIndex = 10;
+            this.btnTimDonGia.Text = "Tìm";
+            this.btnTimDonGia.UseVisualStyleBackColor = true;
+            this.btnTimDonGia.Click += new System.EventHandler(this.btnTimDonGia_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 390);
+            this.Controls.Add(this.btnTimDonGia);
+            this.Controls.Add(this.txtDonGia);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lvSanPham);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.txtTim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstN);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Học Find";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTim;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.ListView lvSanPham;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDonGia;
+        private System.Windows.Forms.Button btnTimDonGia;
+    }
+}
+
---
+++ HocFind/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HocFind
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<double> dsDouble = new List<double>();
+        List<SanPham> dsSanPham = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(txtN.Text);
+            Random rd = new Random();
+            dsDouble.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                double x = Math.Round(rd.NextDouble() * 100, 2);
+                dsDouble.Add(x);
+            }
+            lstN.Items.Clear();
+            dsDouble.ForEach(x => lstN.Items.Add(x));
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            double k = double.Parse(txtTim.Text);
+            double kq = dsDouble.Find(x => x == k);
+            MessageBox.Show(kq + "");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dsSanPham = new List<SanPham>();
+            dsSanPham.Add(new SanPham() { Ma = "SP1", Ten = "Coca cola", DonGia = 150 });
+            dsSanPham.Add(new SanPham() { Ma = "SP2", Ten = "Pepsi", DonGia = 140 });
+            dsSanPham.Add(new SanPham() { Ma = "SP3", Ten = "Red bull", DonGia = 140 });
+            dsSanPham.Add(new SanPham() { Ma = "SP4", Ten = "caffe", DonGia = 130 });
+            dsSanPham.Add(new SanPham() { Ma = "SP5", Ten = "Lipton", DonGia = 160 });
+            dsSanPham.Add(new SanPham() { Ma = "SP6", Ten = "Tra chanh", DonGia = 110 });
+            HienThiLenLV();
+        }
+
+        private void HienThiLenLV()
+        {
+            dsSanPham.ForEach(x =>
+            {
+                ListViewItem lvi = new ListViewItem(x.Ma);
+                lvi.SubItems.Add(x.Ten);
+                lvi.SubItems.Add(x.DonGia + "");
+                lvSanPham.Items.Add(lvi);
+            });
+        }
+
+        private void btnTimDonGia_Click(object sender, EventArgs e)
+        {
+            double gia = double.Parse(txtDonGia.Text);
+            SanPham sp = dsSanPham.Find(x => x.DonGia == gia);
+            if (sp != null)
+            {
+                MessageBox.Show("Tim thay SP: " + sp.Ten);
+            }
+            else
+            {
+                MessageBox.Show("Khong tim thay");
+            }
+        }
+    }
+}
