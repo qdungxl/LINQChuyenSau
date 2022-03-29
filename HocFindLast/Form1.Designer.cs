1362aac3588a5f098a3430d6e60e3372e8e448f0
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-03-29 21:24:09 +0900

    Add FindLast

---
+++ HocFindLast/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace HocFindLast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.btnTaoDS = new System.Windows.Forms.Button();
+            this.lstDS = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTim = new System.Windows.Forms.TextBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(785, 551);
+            this.splitContainer1.SplitterDistance = 383;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lstDS);
+            this.splitContainer2.Panel1.Controls.Add(this.btnTaoDS);
+            this.splitContainer2.Panel1.Controls.Add(this.txtN);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.txtKQ);
+            this.splitContainer2.Panel2.Controls.Add(this.label3);
+            this.splitContainer2.Panel2.Controls.Add(this.btnTim);
+            this.splitContainer2.Panel2.Controls.Add(this.txtTim);
+            this.splitContainer2.Panel2.Controls.Add(this.label2);
+            this.splitContainer2.Size = new System.Drawing.Size(383, 551);
+            this.splitContainer2.SplitterDistance = 346;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N:";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(95, 33);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(145, 26);
+            this.txtN.TabIndex = 1;
+            // 
+            // btnTaoDS
+            // 
+            this.btnTaoDS.Location = new System.Drawing.Point(271, 23);
+            this.btnTaoDS.Name = "btnTaoDS";
+            this.btnTaoDS.Size = new System.Drawing.Size(93, 35);
+            this.btnTaoDS.TabIndex = 2;
+            this.btnTaoDS.Text = "Tao DS";
+            this.btnTaoDS.UseVisualStyleBackColor = true;
+            this.btnTaoDS.Click += new System.EventHandler(this.btnTaoDS_Click);
+            // 
+            // lstDS
+            // 
+            this.lstDS.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lstDS.FormattingEnabled = true;
+            this.lstDS.ItemHeight = 20;
+            this.lstDS.Location = new System.Drawing.Point(0, 82);
+            this.lstDS.Name = "lstDS";
+            this.lstDS.Size = new System.Drawing.Size(383, 264);
+            this.lstDS.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nhập N cần tìm:";
+            // 
+            // txtTim
+            // 
+            this.txtTim.Location = new System.Drawing.Point(161, 39);
+            this.txtTim.Name = "txtTim";
+            this.txtTim.Size = new System.Drawing.Size(100, 26);
+            this.txtTim.TabIndex = 1;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(267, 26);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(93, 39);
+            this.btnTim.TabIndex = 2;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Kết quả:";
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(198, 99);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(162, 26);
+            this.txtKQ.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 551);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Học FindLast";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListBox lstDS;
+        private System.Windows.Forms.Button btnTaoDS;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.TextBox txtTim;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HocFindLast/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HocFindLast
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<double> dsNguon = new List<double>();
+        Random rd = new Random();
+        private void btnTaoDS_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(txtN.Text);
+            for (int i =0; i < n; i++)
+            {
+                double x = Math.Round(rd.NextDouble() * 100, 2);
+                dsNguon.Add(x);
+            }
+            lstDS.Items.Clear();
+            dsNguon.ForEach(x =>
+            {
+                lstDS.Items.Add(x);
+            });
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            double k = double.Parse(txtTim.Text);
+            double kq = dsNguon.FindLast(x => x == k);
+            txtKQ.Text = kq + "";
+        }
+    }
+}
