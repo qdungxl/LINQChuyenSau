e526370b5ecf243cfa8c4bfdf9159fc606474a01
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-03-30 20:59:56 +0900

    Add RemoveAll

---
+++ HocRemoveAll/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace HocRemoveAll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.btnTaoDS = new System.Windows.Forms.Button();
+            this.lstDS = new System.Windows.Forms.ListBox();
+            this.btnXoaSoLe = new System.Windows.Forms.Button();
+            this.lvSanPham = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtXuatXu = new System.Windows.Forms.TextBox();
+            this.btnXoa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnXoaSoLe);
+            this.splitContainer1.Panel1.Controls.Add(this.lstDS);
+            this.splitContainer1.Panel1.Controls.Add(this.btnTaoDS);
+            this.splitContainer1.Panel1.Controls.Add(this.txtN);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnXoa);
+            this.splitContainer1.Panel2.Controls.Add(this.txtXuatXu);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.txtMax);
+            this.splitContainer1.Panel2.Controls.Add(this.txtMin);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.lvSanPham);
+            this.splitContainer1.Size = new System.Drawing.Size(768, 438);
+            this.splitContainer1.SplitterDistance = 328;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N:";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(83, 30);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 26);
+            this.txtN.TabIndex = 1;
+            // 
+            // btnTaoDS
+            // 
+            this.btnTaoDS.Location = new System.Drawing.Point(219, 12);
+            this.btnTaoDS.Name = "btnTaoDS";
+            this.btnTaoDS.Size = new System.Drawing.Size(93, 43);
+            this.btnTaoDS.TabIndex = 2;
+            this.btnTaoDS.Text = "Tạo DS";
+            this.btnTaoDS.UseVisualStyleBackColor = true;
+            this.btnTaoDS.Click += new System.EventHandler(this.btnTaoDS_Click);
+            // 
+            // lstDS
+            // 
+            this.lstDS.FormattingEnabled = true;
+            this.lstDS.ItemHeight = 20;
+            this.lstDS.Location = new System.Drawing.Point(13, 88);
+            this.lstDS.Name = "lstDS";
+            this.lstDS.Size = new System.Drawing.Size(299, 184);
+            this.lstDS.TabIndex = 3;
+            // 
+            // btnXoaSoLe
+            // 
+            this.btnXoaSoLe.Location = new System.Drawing.Point(16, 298);
+            this.btnXoaSoLe.Name = "btnXoaSoLe";
+            this.btnXoaSoLe.Size = new System.Drawing.Size(116, 44);
+            this.btnXoaSoLe.TabIndex = 4;
+            this.btnXoaSoLe.Text = "Xóa số lẻ";
+            this.btnXoaSoLe.UseVisualStyleBackColor = true;
+            this.btnXoaSoLe.Click += new System.EventHandler(this.btnXoaSoLe_Click);
+            // 
+            // lvSanPham
+            // 
+            this.lvSanPham.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvSanPham.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lvSanPham.FullRowSelect = true;
+            this.lvSanPham.GridLines = true;
+            this.lvSanPham.HideSelection = false;
+            this.lvSanPham.Location = new System.Drawing.Point(0, 0);
+            this.lvSanPham.Name = "lvSanPham";
+            this.lvSanPham.Size = new System.Drawing.Size(436, 210);
+            this.lvSanPham.TabIndex = 0;
+            this.lvSanPham.UseCompatibleStateImageBehavior = false;
+            this.lvSanPham.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Đơn Giá";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Xuất xứ";
+            this.columnHeader4.Width = 100;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Giá min:";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(99, 229);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(88, 26);
+            this.txtMin.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Giá max:";
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(311, 229);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(88, 26);
+            this.txtMax.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Xuất xứ:";
+            // 
+            // txtXuatXu
+            // 
+            this.txtXuatXu.Location = new System.Drawing.Point(115, 304);
+            this.txtXuatXu.Name = "txtXuatXu";
+            this.txtXuatXu.Size = new System.Drawing.Size(106, 26);
+            this.txtXuatXu.TabIndex = 4;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(259, 288);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(106, 44);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 438);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Học RemoveAll";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnXoaSoLe;
+        private System.Windows.Forms.ListBox lstDS;
+        private System.Windows.Forms.Button btnTaoDS;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lvSanPham;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.TextBox txtXuatXu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HocRemoveAll/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HocRemoveAll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> dsInt = new List<int>();
+        Random rd = new Random();
+        List<SanPham> dsSP = new List<SanPham>();
+        private void btnTaoDS_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(txtN.Text);
+            for (int i =0; i < n; i++)
+            {
+                dsInt.Add(rd.Next(100));
+            }
+            lstDS.Items.Clear();
+            dsInt.ForEach(x => { lstDS.Items.Add(x); });
+        }
+
+        private void btnXoaSoLe_Click(object sender, EventArgs e)
+        {
+            dsInt.RemoveAll(x => x % 2 != 0);
+            lstDS.Items.Clear();
+            dsInt.ForEach(x => { lstDS.Items.Add(x); });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dsSP.Add(new SanPham() { Ma = "SP1", Ten = "Dien thoai SamSung", DonGia = 100, XuatXu = "Han Quoc" });
+            dsSP.Add(new SanPham() { Ma = "SP2", Ten = "Dien thoai LG", DonGia = 200, XuatXu = "Han Quoc" });
+            dsSP.Add(new SanPham() { Ma = "SP3", Ten = "Dien thoai HTC", DonGia = 300, XuatXu = "Dai Loan" });
+            dsSP.Add(new SanPham() { Ma = "SP4", Ten = "Dien thoai RedMi", DonGia = 400, XuatXu = "Trung Quoc" });
+            dsSP.Add(new SanPham() { Ma = "SP5", Ten = "Dien thoai Xiaomi", DonGia = 500, XuatXu = "Trung Quoc" });
+            lstDS.Items.Clear();
+            dsSP.ForEach(x =>
+            {
+                ShowOneItemOfListOnListView(x);
+            });
+        }
+
+        private void ShowOneItemOfListOnListView(SanPham x)
+        {
+            ListViewItem lvi = new ListViewItem(x.Ma);
+            lvi.SubItems.Add(x.Ten);
+            lvi.SubItems.Add(x.DonGia+"");
+            lvi.SubItems.Add(x.XuatXu);
+            lvSanPham.Items.Add(lvi);
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            int min = int.Parse(txtMin.Text);
+            int max = int.Parse(txtMax.Text);
+            string XuatXu = txtXuatXu.Text;
+            dsSP.RemoveAll(x => x.DonGia >= min && x.DonGia <= max && x.XuatXu == XuatXu);
+            lvSanPham.Items.Clear();
+            dsSP.ForEach(x =>
+            {
+                ShowOneItemOfListOnListView(x);
+            });
+        }
+    }
+}
