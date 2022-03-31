fffd529bff064b26f7bb9c2e23dcec738f0440fd
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-03-31 21:24:13 +0900

    Add Where

---
+++ HocWhere/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace HocWhere
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lvGoc = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dgvSinhVien = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDauSo = new System.Windows.Forms.TextBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.btnC2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSinhVien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lvGoc);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(610, 592);
+            this.splitContainer1.SplitterDistance = 218;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.btnC2);
+            this.splitContainer2.Panel1.Controls.Add(this.btnTim);
+            this.splitContainer2.Panel1.Controls.Add(this.txtDauSo);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.dgvSinhVien);
+            this.splitContainer2.Size = new System.Drawing.Size(610, 370);
+            this.splitContainer2.SplitterDistance = 120;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // lvGoc
+            // 
+            this.lvGoc.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvGoc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvGoc.FullRowSelect = true;
+            this.lvGoc.GridLines = true;
+            this.lvGoc.HideSelection = false;
+            this.lvGoc.Location = new System.Drawing.Point(0, 0);
+            this.lvGoc.Name = "lvGoc";
+            this.lvGoc.Size = new System.Drawing.Size(610, 218);
+            this.lvGoc.TabIndex = 0;
+            this.lvGoc.UseCompatibleStateImageBehavior = false;
+            this.lvGoc.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Email";
+            this.columnHeader3.Width = 250;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Phone";
+            this.columnHeader4.Width = 150;
+            // 
+            // dgvSinhVien
+            // 
+            this.dgvSinhVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSinhVien.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSinhVien.Location = new System.Drawing.Point(0, 0);
+            this.dgvSinhVien.Name = "dgvSinhVien";
+            this.dgvSinhVien.Size = new System.Drawing.Size(610, 246);
+            this.dgvSinhVien.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập đầu số:";
+            // 
+            // txtDauSo
+            // 
+            this.txtDauSo.Location = new System.Drawing.Point(153, 45);
+            this.txtDauSo.Name = "txtDauSo";
+            this.txtDauSo.Size = new System.Drawing.Size(169, 26);
+            this.txtDauSo.TabIndex = 1;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(361, 31);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(94, 40);
+            this.btnTim.TabIndex = 2;
+            this.btnTim.Text = "Tìm C1";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // btnC2
+            // 
+            this.btnC2.Location = new System.Drawing.Point(474, 31);
+            this.btnC2.Name = "btnC2";
+            this.btnC2.Size = new System.Drawing.Size(94, 40);
+            this.btnC2.TabIndex = 3;
+            this.btnC2.Text = "Tìm C2";
+            this.btnC2.UseVisualStyleBackColor = true;
+            this.btnC2.Click += new System.EventHandler(this.btnC2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 592);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Học Where";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSinhVien)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView lvGoc;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.TextBox txtDauSo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvSinhVien;
+        private System.Windows.Forms.Button btnC2;
+    }
+}
+
---
+++ HocWhere/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HocWhere
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<SinhVien> dsSV = new List<SinhVien>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dsSV.Add(new SinhVien() { Ma = "SV1", Ten = "Mai Quoc Dung", Email = "maiqucodung95@gmail.com", Phone = "090111222" });
+            dsSV.Add(new SinhVien() { Ma = "SV2", Ten = "Mai Quoc Huy", Email = "maiqucoduy95@gmail.com", Phone = "090111333" });
+            dsSV.Add(new SinhVien() { Ma = "SV3", Ten = "Mai Quoc Hieu", Email = "maiqucohieu95@gmail.com", Phone = "090111444" });
+            dsSV.Add(new SinhVien() { Ma = "SV4", Ten = "Mai Quoc Duy", Email = "maiqucoduy95@gmail.com", Phone = "090111555" });
+            dsSV.Add(new SinhVien() { Ma = "SV5", Ten = "Mai Quoc Hung", Email = "maiquchung95@gmail.com", Phone = "090111666" });
+            dsSV.Add(new SinhVien() { Ma = "SV6", Ten = "Mai Quoc Tuan", Email = "maiqucotuan95@gmail.com", Phone = "090111777" });
+            dsSV.Add(new SinhVien() { Ma = "SV7", Ten = "Mai Quoc Anh", Email = "maiqucoanh95@gmail.com", Phone = "090111888" });
+            dsSV.ForEach(x =>
+            {
+                ShowOneSinhVienOnListView(x);
+            });
+        }
+
+        private void ShowOneSinhVienOnListView(SinhVien x)
+        {
+            ListViewItem lvi = new ListViewItem(x.Ma);
+            lvi.SubItems.Add(x.Ten);
+            lvi.SubItems.Add(x.Email);
+            lvi.SubItems.Add(x.Phone);
+            lvGoc.Items.Add(lvi);
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            var dsLoc = dsSV.Where(sv => sv.Phone.EndsWith(txtDauSo.Text)).ToList();
+            dgvSinhVien.DataSource = dsLoc;
+        }
+
+        private void btnC2_Click(object sender, EventArgs e)
+        {
+            var dsLoc = (from sv in dsSV where sv.Phone.EndsWith(txtDauSo.Text) select sv).ToList();
+            dgvSinhVien.DataSource = dsLoc;
+        }
+    }
+}
