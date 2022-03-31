94fd723c786302c65781f71b3315187c00f2ef2c
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-03-31 20:55:33 +0900

    Add Select

---
+++ HocSelect/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace HocSelect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lvGoc = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoc = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lvGoc);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1039, 418);
+            this.splitContainer1.SplitterDistance = 402;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            this.splitContainer2.Panel1.Controls.Add(this.btnLoc);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer2.Size = new System.Drawing.Size(633, 418);
+            this.splitContainer2.SplitterDistance = 211;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // lvGoc
+            // 
+            this.lvGoc.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvGoc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvGoc.FullRowSelect = true;
+            this.lvGoc.GridLines = true;
+            this.lvGoc.HideSelection = false;
+            this.lvGoc.Location = new System.Drawing.Point(0, 0);
+            this.lvGoc.Name = "lvGoc";
+            this.lvGoc.Size = new System.Drawing.Size(402, 418);
+            this.lvGoc.TabIndex = 0;
+            this.lvGoc.UseCompatibleStateImageBehavior = false;
+            this.lvGoc.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã";
+            this.columnHeader1.Width = 40;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Email";
+            this.columnHeader3.Width = 150;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(418, 418);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnLoc
+            // 
+            this.btnLoc.Location = new System.Drawing.Point(33, 42);
+            this.btnLoc.Name = "btnLoc";
+            this.btnLoc.Size = new System.Drawing.Size(165, 46);
+            this.btnLoc.TabIndex = 0;
+            this.btnLoc.Text = "Lọc sinh viên C1";
+            this.btnLoc.UseVisualStyleBackColor = true;
+            this.btnLoc.Click += new System.EventHandler(this.btnLoc_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Lọc sinh viên C2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 418);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Học Select";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView lvGoc;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button btnLoc;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HocSelect/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HocSelect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<SinhVien> dsSV = new List<SinhVien>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dsSV.Add(new SinhVien() { Ma = "SV1", Ten = "Mai Quoc Dung", Email = "maiqucodung95@gmail.com" });
+            dsSV.Add(new SinhVien() { Ma = "SV2", Ten = "Mai Quoc Huy", Email = "maiqucoduy95@gmail.com" });
+            dsSV.Add(new SinhVien() { Ma = "SV3", Ten = "Mai Quoc Hieu", Email = "maiqucohieu95@gmail.com" });
+            dsSV.Add(new SinhVien() { Ma = "SV4", Ten = "Mai Quoc Duy", Email = "maiqucoduy95@gmail.com" });
+            dsSV.Add(new SinhVien() { Ma = "SV5", Ten = "Mai Quoc Hung", Email = "maiquchung95@gmail.com" });
+            dsSV.Add(new SinhVien() { Ma = "SV6", Ten = "Mai Quoc Tuan", Email = "maiqucotuan95@gmail.com" });
+            dsSV.Add(new SinhVien() { Ma = "SV7", Ten = "Mai Quoc Anh", Email = "maiqucoanh95@gmail.com" });
+            lvGoc.Items.Clear();
+            dsSV.ForEach(x =>
+            {
+                ShowOneSinhVienOnListView(x);
+            });
+        }
+
+        private void ShowOneSinhVienOnListView(SinhVien x)
+        {
+            ListViewItem lvi = new ListViewItem(x.Ma);
+            lvi.SubItems.Add(x.Ten);
+            lvi.SubItems.Add(x.Email);
+            lvGoc.Items.Add(lvi);
+        }
+
+        private void btnLoc_Click(object sender, EventArgs e)
+        {
+            var dsLoc = dsSV.Select(x => new { x.Ma, x.Ten }).ToList();
+            dataGridView1.DataSource = dsLoc;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dsLoc = (from x in dsSV select new { x.Ma, x.Ten }).ToList();
+            dataGridView1.DataSource = dsLoc;
+        }
+    }
+}
