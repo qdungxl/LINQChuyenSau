e6719bdc539b980aaed5ad4cf5e57496551b1bf0
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-02 21:42:11 +0900

    Add TakeWhile

---
+++ HocTakeWhile/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace HocTakeWhile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.btnTaoDS = new System.Windows.Forms.Button();
+            this.lstN = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTakeWhile = new System.Windows.Forms.Button();
+            this.lstTakeWhile = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N:";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(78, 23);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(89, 26);
+            this.txtN.TabIndex = 1;
+            // 
+            // btnTaoDS
+            // 
+            this.btnTaoDS.Location = new System.Drawing.Point(185, 13);
+            this.btnTaoDS.Name = "btnTaoDS";
+            this.btnTaoDS.Size = new System.Drawing.Size(86, 36);
+            this.btnTaoDS.TabIndex = 2;
+            this.btnTaoDS.Text = "Tạo DS";
+            this.btnTaoDS.UseVisualStyleBackColor = true;
+            this.btnTaoDS.Click += new System.EventHandler(this.btnTaoDS_Click);
+            // 
+            // lstN
+            // 
+            this.lstN.FormattingEnabled = true;
+            this.lstN.ItemHeight = 20;
+            this.lstN.Location = new System.Drawing.Point(28, 64);
+            this.lstN.Name = "lstN";
+            this.lstN.Size = new System.Drawing.Size(243, 264);
+            this.lstN.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "TakeWhile:";
+            // 
+            // btnTakeWhile
+            // 
+            this.btnTakeWhile.Location = new System.Drawing.Point(417, 12);
+            this.btnTakeWhile.Name = "btnTakeWhile";
+            this.btnTakeWhile.Size = new System.Drawing.Size(183, 41);
+            this.btnTakeWhile.TabIndex = 6;
+            this.btnTakeWhile.Text = "TakeWhile";
+            this.btnTakeWhile.UseVisualStyleBackColor = true;
+            this.btnTakeWhile.Click += new System.EventHandler(this.btnTakeWhile_Click);
+            // 
+            // lstTakeWhile
+            // 
+            this.lstTakeWhile.FormattingEnabled = true;
+            this.lstTakeWhile.ItemHeight = 20;
+            this.lstTakeWhile.Location = new System.Drawing.Point(302, 64);
+            this.lstTakeWhile.Name = "lstTakeWhile";
+            this.lstTakeWhile.Size = new System.Drawing.Size(296, 264);
+            this.lstTakeWhile.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 350);
+            this.Controls.Add(this.lstTakeWhile);
+            this.Controls.Add(this.btnTakeWhile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstN);
+            this.Controls.Add(this.btnTaoDS);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Học TakeWhile";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Button btnTaoDS;
+        private System.Windows.Forms.ListBox lstN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTakeWhile;
+        private System.Windows.Forms.ListBox lstTakeWhile;
+    }
+}
+
---
+++ HocTakeWhile/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HocTakeWhile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        List<int> dsInt = null;
+        Random rd = new Random();
+        private void btnTaoDS_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(txtN.Text);
+            dsInt = new List<int>();
+            for (int i=0; i< n; i++)
+            {
+                int x = rd.Next(100);
+                dsInt.Add(x);
+            }
+            lstN.Items.Clear();
+            dsInt.ForEach(x => lstN.Items.Add(x));
+        }
+
+        private void btnTakeWhile_Click(object sender, EventArgs e)
+        {
+            var dsKQ = dsInt.TakeWhile(x => x % 2 != 0).ToList();
+            lstTakeWhile.Items.Clear();
+            dsKQ.ForEach(x => lstTakeWhile.Items.Add(x));
+
+        }
+    }
+}
