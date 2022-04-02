a7dba17d5e0652d4a3aa42c0b7d3481424c5d73d
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-02 15:22:47 +0900

    Add MaxMin

---
+++ HocMaxMin/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace HocMaxMin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.btnTao = new System.Windows.Forms.Button();
+            this.lvSo = new System.Windows.Forms.ListBox();
+            this.btnMax = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N:";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(154, 29);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(148, 26);
+            this.txtN.TabIndex = 1;
+            // 
+            // btnTao
+            // 
+            this.btnTao.Location = new System.Drawing.Point(349, 12);
+            this.btnTao.Name = "btnTao";
+            this.btnTao.Size = new System.Drawing.Size(116, 43);
+            this.btnTao.TabIndex = 2;
+            this.btnTao.Text = "Tạo";
+            this.btnTao.UseVisualStyleBackColor = true;
+            // 
+            // lvSo
+            // 
+            this.lvSo.FormattingEnabled = true;
+            this.lvSo.ItemHeight = 20;
+            this.lvSo.Location = new System.Drawing.Point(45, 91);
+            this.lvSo.Name = "lvSo";
+            this.lvSo.Size = new System.Drawing.Size(257, 244);
+            this.lvSo.TabIndex = 3;
+            // 
+            // btnMax
+            // 
+            this.btnMax.Location = new System.Drawing.Point(349, 91);
+            this.btnMax.Name = "btnMax";
+            this.btnMax.Size = new System.Drawing.Size(116, 45);
+            this.btnMax.TabIndex = 4;
+            this.btnMax.Text = "Max";
+            this.btnMax.UseVisualStyleBackColor = true;
+            // 
+            // btnMin
+            // 
+            this.btnMin.Location = new System.Drawing.Point(349, 152);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(116, 45);
+            this.btnMin.TabIndex = 4;
+            this.btnMin.Text = "Min";
+            this.btnMin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 376);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnMax);
+            this.Controls.Add(this.lvSo);
+            this.Controls.Add(this.btnTao);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Học MaxMin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Button btnTao;
+        private System.Windows.Forms.ListBox lvSo;
+        private System.Windows.Forms.Button btnMax;
+        private System.Windows.Forms.Button btnMin;
+    }
+}
+
