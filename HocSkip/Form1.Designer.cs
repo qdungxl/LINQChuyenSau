71c468817876ce7eb5744c0474ac8854f907acdb
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-02 16:10:52 +0900

    Add Skip

---
+++ HocSkip/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace HocSkip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstDSGoc = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSkip = new System.Windows.Forms.TextBox();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.lstDSSkip = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lstDSGoc);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.txtN);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lstDSSkip);
+            this.splitContainer1.Panel2.Controls.Add(this.btnSkip);
+            this.splitContainer1.Panel2.Controls.Add(this.txtSkip);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Size = new System.Drawing.Size(827, 450);
+            this.splitContainer1.SplitterDistance = 411;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N:";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(104, 46);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(178, 26);
+            this.txtN.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tạo DS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstDSGoc
+            // 
+            this.lstDSGoc.FormattingEnabled = true;
+            this.lstDSGoc.ItemHeight = 20;
+            this.lstDSGoc.Location = new System.Drawing.Point(52, 115);
+            this.lstDSGoc.Name = "lstDSGoc";
+            this.lstDSGoc.Size = new System.Drawing.Size(327, 304);
+            this.lstDSGoc.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Skip";
+            // 
+            // txtSkip
+            // 
+            this.txtSkip.Location = new System.Drawing.Point(133, 46);
+            this.txtSkip.Name = "txtSkip";
+            this.txtSkip.Size = new System.Drawing.Size(145, 26);
+            this.txtSkip.TabIndex = 1;
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Location = new System.Drawing.Point(299, 33);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(101, 38);
+            this.btnSkip.TabIndex = 2;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = true;
+            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
+            // 
+            // lstDSSkip
+            // 
+            this.lstDSSkip.FormattingEnabled = true;
+            this.lstDSSkip.ItemHeight = 20;
+            this.lstDSSkip.Location = new System.Drawing.Point(47, 115);
+            this.lstDSSkip.Name = "lstDSSkip";
+            this.lstDSSkip.Size = new System.Drawing.Size(353, 304);
+            this.lstDSSkip.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Học Skip";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox lstDSGoc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstDSSkip;
+        private System.Windows.Forms.Button btnSkip;
+        private System.Windows.Forms.TextBox txtSkip;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HocSkip/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HocSkip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> dsInt = null;
+        Random rd = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dsInt = new List<int>();
+            int n = int.Parse(txtN.Text);
+            for(int i =0; i < n; i++)
+            {
+                int x = rd.Next(100);
+                dsInt.Add(x);
+            }
+            lstDSGoc.Items.Clear();
+            dsInt.ForEach(x =>
+            {
+                lstDSGoc.Items.Add(x);
+            });
+        }
+
+        private void btnSkip_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(txtSkip.Text);
+            var dsMoi = dsInt.Skip(n).ToList();
+            lstDSSkip.Items.Clear();
+            dsMoi.ForEach(x => lstDSSkip.Items.Add(x));
+        }
+    }
+}
